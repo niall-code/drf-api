b2b09d22fc6c4761f59781541919908786559769
Niall Caplis <niallcaplis@gmail.com>
2024-09-19 14:37:37 +0000

    prevent editing of other users' profiles

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -4,10 +4,22 @@ from .models import Profile
 
 class ProfileSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
+    #
+    is_owner = serializers.SerializerMethodField()
+
+    def get_is_owner(self, obj):
+        '''
+        Accesses context obj that views passed into serializer
+        '''
+        request = self.context['request']
+        # Returns True if user is object owner
+        # Feeds into variable above, then displayed with Meta class below
+        return request.user == obj.owner
+    #
 
     class Meta:
         model = Profile
         fields = [
-            # or '__all__'
-            'id', 'owner', 'created_at', 'updated_at', 'name', 'content', 'image'
+            # or '__all__' (but 'is_owner' is additional)
+            'id', 'owner', 'created_at', 'updated_at', 'name', 'content', 'image', 'is_owner'
         ]
--- profiles/views.py
+++ profiles/views.py
@@ -4,23 +4,31 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Profile
 from .serializers import ProfileSerializer
+from drf_api.permissions import IsOwnerOrReadOnly
 
 
 class ProfileList(APIView):
     def get(self, request):
         profiles = Profile.objects.all()
-        serializer = ProfileSerializer(profiles, many=True)
+        serializer = ProfileSerializer(
+            profiles,
+            many=True,
+            context={'request': request}
+        )
         return Response(serializer.data)
 
 
 class ProfileDetail(APIView):
     serializer_class = ProfileSerializer
+    permission_classes = [IsOwnerOrReadOnly]
+
     def get_object(self, pk):
         '''
         Try to get path-specified profile
         '''
         try:
             profile = Profile.objects.get(pk=pk)
+            self.check_object_permissions(self.request, profile)
             return profile
         except Profile.DoesNotExist:
             raise Http404
@@ -30,7 +38,10 @@ class ProfileDetail(APIView):
         Show profile data
         '''
         profile = self.get_object(pk)
-        serializer = ProfileSerializer(profile)
+        serializer = ProfileSerializer(
+            profile,
+            context={'request': request}
+        )
         return Response(serializer.data)
 
     def put(self, request, pk):
@@ -38,7 +49,11 @@ class ProfileDetail(APIView):
         Editing the profile
         '''
         profile = self.get_object(pk)
-        serializer = ProfileSerializer(profile, data=request.data)
+        serializer = ProfileSerializer(
+            profile,
+            data=request.data,
+            context={'request': request}
+        )
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
