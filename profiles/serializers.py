772e6ba4066af06cc21ef3c3e0a7e9888de87022
Niall Caplis <niallcaplis@gmail.com>
2024-09-23 14:16:06 +0000

    in serializers, add follower_id for profiles and like_id for posts

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -1,11 +1,13 @@
 from rest_framework import serializers
 from .models import Profile
+from followers.models import Follower
 
 
 class ProfileSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     #
     is_owner = serializers.SerializerMethodField()
+    following_id = serializers.SerializerMethodField()
 
     def get_is_owner(self, obj):
         '''
@@ -17,9 +19,20 @@ class ProfileSerializer(serializers.ModelSerializer):
         return request.user == obj.owner
     #
 
+    def get_following_id(self, obj):
+        user = self.context['request'].user
+        if user.is_authenticated:
+            following = Follower.objects.filter(
+                owner=user, followed=obj.owner
+            ).first()
+            # print(following)
+            return following.id if following else None
+        return None
+
     class Meta:
         model = Profile
         fields = [
             # or '__all__' (but 'is_owner' is additional)
-            'id', 'owner', 'created_at', 'updated_at', 'name', 'content', 'image', 'is_owner'
+            'id', 'owner', 'created_at', 'updated_at', 'name',
+            'content', 'image', 'is_owner', 'following_id',
         ]
