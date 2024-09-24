a6a8a902f7732384d919990316cd82c46858c1cf
Niall Caplis <niallcaplis@gmail.com>
2024-09-24 13:40:14 +0000

    specify querysets in profiles/views.py

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -8,6 +8,9 @@ class ProfileSerializer(serializers.ModelSerializer):
     #
     is_owner = serializers.SerializerMethodField()
     following_id = serializers.SerializerMethodField()
+    posts_count = serializers.ReadOnlyField()
+    followers_count = serializers.ReadOnlyField()
+    following_count = serializers.ReadOnlyField()
 
     def get_is_owner(self, obj):
         '''
@@ -35,4 +38,5 @@ class ProfileSerializer(serializers.ModelSerializer):
             # or '__all__' (but 'is_owner' is additional)
             'id', 'owner', 'created_at', 'updated_at', 'name',
             'content', 'image', 'is_owner', 'following_id',
+            'posts_count', 'followers_count', 'following_count',
         ]
