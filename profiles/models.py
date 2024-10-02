9108f13c5bbd96b881cdff28673c9f64329c4b58
Niall Caplis <niallcaplis@gmail.com>
2024-10-02 20:21:45 +0000

    alter Profile model image field to use Cloudinary

--- profiles/models.py
+++ profiles/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.db.models.signals import post_save
 from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
 
 class Profile(models.Model):
@@ -9,9 +10,10 @@ class Profile(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     name = models.CharField(max_length=255, blank=True)
     content = models.TextField(blank=True)
-    image = models.ImageField(
-        upload_to='images/', default='../default_profile_vbmyq5'
-    )
+    image = CloudinaryField('image', default='default_profile_vbmyq5')
+    # image = models.ImageField(
+    #     upload_to='images/', default='../default_profile_vbmyq5'
+    # )
 
     class Meta:
         ordering = ['-created_at']
