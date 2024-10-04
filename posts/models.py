786452eaac9eadd224ed6a877b1d4a67e4aa2f74
Niall Caplis <niallcaplis@gmail.com>
2024-10-04 20:01:06 +0000

    reverse Post model change, downgrade Django, dependency changes

--- posts/models.py
+++ posts/models.py
@@ -30,11 +30,8 @@ class Post(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     title = models.CharField(max_length=255)
     content = models.TextField(blank=True)
-    # image = models.ImageField(
-    #     upload_to='images/', default='../default_post_dhimcq', blank=True
-    # )
-    image = CloudinaryField(
-        'image', default='default_post_dhimcq', blank=True
+    image = models.ImageField(
+        upload_to='images/', default='../default_post_dhimcq', blank=True
     )
     image_filter = models.CharField(
         max_length=32, choices=image_filter_choices, default='normal'
